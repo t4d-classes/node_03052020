42056c1b85a9298d0146bda2d287bdf0eb1d6e1b
Eric Greene <eric@t4d.io>
2020-03-06 17:37:06 -0500

    add code demos

---
+++ web-app/exercises/exercise-4.md
@@ -0,0 +1,11 @@
+# Exercise 4
+
+1. Implement the delete and replace operations for the REST API
+
+https://en.wikipedia.org/wiki/Representational_state_transfer
+
+2. Test the end points with your REST client
+
+3. For the return values of both, its ok to return No Content.
+
+4. Ensure it works!
--- web-app/server/src/index.js
+++ web-app/server/src/index.js
@@ -9,16 +9,72 @@ const app = express();
 app.use('/api', bodyParser.json());
 
 app.post('/api/colors', (req, res) => {
-  console.log('write the color to the file', req.body);
 
-  res.json(req.body);
-})
+  fs.promises.readFile('./db.json')
+    .then(data => {
+
+      const db = JSON.parse(data);
+      const newColor = {
+        ...req.body,
+        id: Math.max(...db.colors.map(c => c.id), 0) + 1,
+      };
+
+      db.colors.push(newColor);
+
+      return fs.promises.writeFile('./db.json', JSON.stringify(db))
+        .then(() => newColor);
+    })
+    .then((newColor) => {
+      res.json(newColor);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+      res.end();
+    });
+
+});
+
+app.get('/api/colors/:colorId', (req, res) => {
+
+  fs.readFile('./db.json', (err, data) => {
+
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      res.end();
+    }
+
+    const db = JSON.parse(data);
+
+    const color = db.colors.find(
+      c => c.id === Number(req.params.colorId)
+    );
+    
+    if (!color) {
+      res.sendStatus(404);
+      res.end();
+    }
+    
+    res.json(color);
+
+  });
+
+});
 
 app.get('/api/colors', (req, res) => {
 
   fs.readFile('./db.json', (err, data) => {
+
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+      res.end();
+    }
+
     const db = JSON.parse(data);
     res.json(db.colors);
+
   });
 
 });
